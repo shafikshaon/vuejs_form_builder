d634e62248ddcb6ebc34d35314012fcb31a48b69
shafik <shafikshaon@gmail.com>
2020-03-04 00:21:06 +0600

    api single select type input field component prepare

--- src/components/ComponentTypes.js
+++ src/components/ComponentTypes.js
@@ -2,14 +2,14 @@ import FormTemplate from "./FormTemplate.vue"
 import InputBox from "./FormElements/Fields/InputBox.vue"
 import TextArea from "./FormElements/Fields/TextArea.vue"
 import SingleSelect from "./FormElements/Fields/SingleSelect.vue"
-import CheckBox from "./FormElements/Fields/CheckBox.vue"
+import APISingleSelect from "./FormElements/Fields/APISingleSelect";
 
 const COMPONENT_MAP = {
     formTemplate: FormTemplate,
     text: InputBox,
     textarea: TextArea,
     single_select: SingleSelect,
-    check: CheckBox
+    api_single_select: APISingleSelect,
 }
 
 export function getComponent(type) {
---
+++ src/components/FormElements/Fields/APISingleSelect.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-select
+                :filterable="false"
+                :options="options"
+                :placeholder="options.attrs.placeholder"
+                @search="onSearch"
+                v-model="selectedValue"
+        ></v-select>
+    </div>
+</template>
+<script>
+    export default {
+        props: ["value"],
+        data() {
+            return {selectedValue: '', options: [{}]}
+        },
+        methods: {
+            onSearch(search) {
+                console.log(search)
+            }
+        },
+        watch: {
+            selectedValue(newVal) {
+                // eslint-disable-next-line no-console
+                this.$emit('input', newVal)
+            }
+        }
+    }
+</script>
